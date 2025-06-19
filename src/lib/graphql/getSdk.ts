052dba14ac412bc336c7e43abf50bf77630431db
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-06-18 21:39:48 -0500

    refactor: use path aliases where applicable

--- src/lib/graphql/getSdk.ts
+++ src/lib/graphql/getSdk.ts
@@ -1,23 +1,13 @@
-import { getSdk as getGraphQLSdk } from "generated/graphql.sdk";
 import { GraphQLClient } from "graphql-request";
-import { API_GRAPHQL_URL } from "lib/config";
 
-import type { Session } from "next-auth";
-
-interface Options {
-  /** Auth session required to retrieve the appropriate `accessToken`.  */
-  session: Session;
-}
+import { getSdk as getGraphQLSdk } from "@/generated/graphql.sdk";
+import { API_GRAPHQL_URL } from "@/lib/config";
 
 /**
  * Utility for getting the GraphQL client SDK.
  */
-const getSdk = ({ session }: Options) => {
-  const graphqlClient = new GraphQLClient(API_GRAPHQL_URL!, {
-    headers: {
-      Authorization: `Bearer ${session.accessToken}`,
-    },
-  });
+const getSdk = () => {
+  const graphqlClient = new GraphQLClient(API_GRAPHQL_URL!);
 
   return getGraphQLSdk(graphqlClient);
 };
