6088f827191ee892d5a23410f71a32dab8925553
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-06-27 18:51:26 -0500

    refactor(app-sidebar): disallow auto collapse, flip cursor resize

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -360,7 +360,7 @@ function SidebarRail({
     setIsDraggingRail,
     widthCookieName: "sidebar:width",
     widthCookieMaxAge: 60 * 60 * 24 * 7, // 1 week
-    enableAutoCollapse: true,
+    enableAutoCollapse: false,
   });
 
   return (
@@ -377,8 +377,8 @@ function SidebarRail({
       // title="Toggle Sidebar"
       className={cn(
         "-translate-x-1/2 group-data-[side=left]:-right-4 absolute inset-y-0 z-20 hidden w-4 transition-all ease-linear after:absolute after:inset-y-0 after:left-1/2 after:w-[2px] group-data-[side=right]:left-0 sm:flex",
-        "in-data-[side=left]:cursor-w-resize in-data-[side=right]:cursor-e-resize",
-        "[[data-side=left][data-state=collapsed]_&]:cursor-e-resize [[data-side=right][data-state=collapsed]_&]:cursor-w-resize",
+        "in-data-[side=left]:cursor-e-resize in-data-[side=right]:cursor-e-resize",
+        "[[data-side=left][data-state=collapsed]_&]:cursor-e-resize [[data-side=right][data-state=collapsed]_&]:cursor-e-resize",
         "group-data-[collapsible=offcanvas]:translate-x-0 hover:group-data-[collapsible=offcanvas]:bg-sidebar group-data-[collapsible=offcanvas]:after:left-full",
         "[[data-side=left][data-collapsible=offcanvas]_&]:-right-2",
         "[[data-side=right][data-collapsible=offcanvas]_&]:-left-2",
