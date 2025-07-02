aff4e0dad563757699b828ebb72c49b246e477f7
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-07-02 14:24:14 -0500

    fix(theme-provider): store current theme locally

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from "@tanstack/react-router";
 import { createServerFn, useServerFn } from "@tanstack/react-start";
 import { setCookie } from "@tanstack/react-start/server";
-import { createContext, use } from "react";
+import { createContext, use, useState } from "react";
 
 import type { PropsWithChildren } from "react";
 
@@ -24,20 +24,31 @@ type Props = PropsWithChildren<{ theme: Theme }>;
 const ThemeContext = createContext<ThemeContextVal | null>(null);
 
 export const ThemeProvider = ({ children, theme }: Props) => {
+  const [currentTheme, setCurrentTheme] = useState(theme);
+
   const router = useRouter();
   const setThemeCookie = useServerFn(setThemeServerFn);
 
   const setTheme = (theme: Theme) => {
     setThemeCookie({ data: theme });
-    // TODO: figure out why this doesn't always invalidate. Seems that you need to toggle twice each time even though the server function is indeed called each time
+
+    theme === "dark" ? setCurrentTheme("dark") : setCurrentTheme("light");
+
     router.invalidate();
   };
 
-  return <ThemeContext value={{ theme, setTheme }}>{children}</ThemeContext>;
+  return (
+    <ThemeContext value={{ theme: currentTheme, setTheme }}>
+      {children}
+    </ThemeContext>
+  );
 };
 
 export const useTheme = () => {
-  const val = use(ThemeContext);
-  if (!val) throw new Error("useTheme called outside of ThemeProvider!");
-  return val;
+  const contextValue = use(ThemeContext);
+
+  if (!contextValue)
+    throw new Error("useTheme must be used within a <ThemeProvider />");
+
+  return contextValue;
 };
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -11,7 +11,7 @@ import { getCookie } from "@tanstack/react-start/server";
 
 import DefaultCatchBoundary from "@/components/layout/DefaultCatchBoundary";
 import seo from "@/lib/util/seo";
-import { ThemeProvider } from "@/providers/ThemeProvider";
+import { ThemeProvider, useTheme } from "@/providers/ThemeProvider";
 import appCss from "@/styles/globals.css?url";
 
 import type { QueryClient } from "@tanstack/react-query";
@@ -21,34 +21,6 @@ import type { Theme } from "@/providers/ThemeProvider";
 const getThemeServerFn = createServerFn().handler(async () => {
   return (getCookie("ui-theme") || "light") as Theme;
 });
-
-const RootDocument = ({ children }: Readonly<{ children: ReactNode }>) => {
-  const theme = Route.useLoaderData();
-
-  return (
-    <html lang="en" className={theme} suppressHydrationWarning>
-      <head>
-        <HeadContent />
-      </head>
-      <body>
-        <ThemeProvider theme={theme}>{children}</ThemeProvider>
-
-        {/* Dev Tools - only included in development */}
-        <TanStackRouterDevtools position="top-right" />
-        <ReactQueryDevtools />
-        <Scripts />
-      </body>
-    </html>
-  );
-};
-
-const RootComponent = () => {
-  return (
-    <RootDocument>
-      <Outlet />
-    </RootDocument>
-  );
-};
 
 export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
   {
@@ -74,3 +46,35 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     component: RootComponent,
   },
 );
+
+function RootComponent() {
+  const theme = Route.useLoaderData();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <RootDocument>
+        <Outlet />
+      </RootDocument>
+    </ThemeProvider>
+  );
+}
+
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+  const { theme } = useTheme();
+
+  return (
+    <html lang="en" className={theme} suppressHydrationWarning>
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+
+        {/* Dev Tools - only included in development */}
+        <TanStackRouterDevtools position="top-right" />
+        <ReactQueryDevtools />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
