44421cc5b4c9ae947202d8a11d981f1bb7eb33d1
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-07-02 09:42:38 -0500

    chore: remove unused source state from useDragStore

--- src/lib/hooks/store/useDragStore.tsx
+++ src/lib/hooks/store/useDragStore.tsx
@@ -2,17 +2,13 @@ import { shallow } from "zustand/shallow";
 import { createWithEqualityFn } from "zustand/traditional";
 
 interface DragStoreState {
-  sourceId: string | null;
   draggableId: string | null;
-  setSourceId: (sourceId: string | null) => void;
   setDraggableId: (draggableId: string | null) => void;
 }
 
 const useDragStore = createWithEqualityFn<DragStoreState>(
   (set) => ({
-    sourceId: null,
     draggableId: null,
-    setSourceId: (sourceId: string | null) => set({ sourceId }),
     setDraggableId: (draggableId: string | null) => set({ draggableId }),
   }),
   shallow,
