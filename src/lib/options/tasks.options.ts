7fef16e418e4f650c653675900f635d6cbea3f55
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-07-10 17:29:44 -0500

    refactor(options): simplify expression for tasks

--- src/lib/options/tasks.options.ts
+++ src/lib/options/tasks.options.ts
@@ -2,42 +2,14 @@ import { queryOptions } from "@tanstack/react-query";
 
 import { useTasksQuery } from "@/generated/graphql";
 
-import type {
-  InputMaybe,
-  TasksQueryVariables,
-  TaskToManyAssigneeFilter,
-  TaskToManyTaskLabelFilter,
-} from "@/generated/graphql";
+import type { TasksQueryVariables } from "@/generated/graphql";
 
-interface Options {
-  projectId: string;
-  search?: string;
-  assignees?: InputMaybe<TaskToManyAssigneeFilter> | undefined;
-  labels?: InputMaybe<TaskToManyTaskLabelFilter> | undefined;
-  priorities?: string[];
-}
-
-const tasksOptions = ({
-  projectId,
-  search,
-  assignees,
-  labels,
-  priorities,
-}: Options) => {
-  const variables: TasksQueryVariables = {
-    projectId,
-    search,
-    assignees,
-    labels,
-    priorities,
-  };
-
-  return queryOptions({
+const tasksOptions = (variables: TasksQueryVariables) =>
+  queryOptions({
     queryKey: useTasksQuery.getKey(variables),
     queryFn: useTasksQuery.fetcher(variables),
     // TODO: discuss proper refetch interval
     refetchInterval: 3000,
   });
-};
 
 export default tasksOptions;
