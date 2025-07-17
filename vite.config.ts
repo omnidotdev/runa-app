857709584e3206614e58666f553f06e3f0c29a38
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-07-17 07:26:03 -0500

    build: update config to use dynamic base url for identity provider cors

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,31 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import mkcert from "vite-plugin-mkcert";
 import tsConfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ command }) => ({
-  server: {
-    port: 3000,
-    // Seemingly needed as a workaround for `https`. See: https://github.com/TanStack/router/issues/4287#issuecomment-2954268842
-    proxy: {},
-    cors: {
-      // TODO: dynamic
-      origin: "https://localhost:8000",
-      credentials: true,
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    server: {
+      port: 3000,
+      // Seemingly needed as a workaround for `https`. See: https://github.com/TanStack/router/issues/4287#issuecomment-2954268842
+      proxy: {},
+      cors: {
+        origin: env.VITE_AUTH_BASE_URL,
+        credentials: true,
+      },
     },
-  },
-  plugins: [
-    // NB: command is `serve` in development, `build` in production
-    command === "serve" && mkcert(),
-    tailwindcss(),
-    tsConfigPaths({ projects: ["./tsconfig.json"] }),
-    tanstackStart({ tsr: { verboseFileRoutes: false }, target: "node-server" }),
-  ],
-}));
+    plugins: [
+      // NB: command is `serve` in development, `build` in production
+      command === "serve" && mkcert(),
+      tailwindcss(),
+      tsConfigPaths({ projects: ["./tsconfig.json"] }),
+      tanstackStart({
+        tsr: { verboseFileRoutes: false },
+        target: "node-server",
+      }),
+    ],
+  };
+});
