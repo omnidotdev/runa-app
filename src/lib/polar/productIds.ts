b7300f8b79e10731b63ae9df88bf40ad06b891d7
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-10-08 10:59:55 -0500

    refactor(polar): add production product IDs

--- src/lib/polar/productIds.ts
+++ src/lib/polar/productIds.ts
@@ -14,13 +14,31 @@ enum SandboxYearly {
   Team = "58d1f183-6f3f-42a0-bbf2-6609c266d0b7",
 }
 
+export enum ProductionFree {
+  Free = "11a3e723-190b-4218-9499-7518f0ced5bc",
+}
+
+enum ProductionMonthly {
+  Basic = "446c2fee-ecde-4aab-94d0-099eafcb011c",
+  Team = "ef5ac069-7b2c-4c58-ab9d-b319f6ba203a",
+}
+
+enum ProductionYearly {
+  Basic = "ef3adfa3-3367-4634-86e2-6f78b119e975",
+  Team = "6906eaa6-b2f5-467f-a622-d018e1d2e9ec",
+}
+
 const sandboxProductIds = [
   SandboxFree.Free,
   ...Object.values(SandboxMonthly),
   ...Object.values(SandboxYearly),
 ];
 
-const productionProductIds: string[] = [];
+const productionProductIds = [
+  ProductionFree.Free,
+  ...Object.values(ProductionMonthly),
+  ...Object.values(ProductionYearly),
+];
 
 const RUNA_PRODUCT_IDS = isDevEnv
   ? [...sandboxProductIds]
