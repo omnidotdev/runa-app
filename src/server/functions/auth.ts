0ad5adbd4722539f2c4e140b47b3d9905aaa49a5
Brian Cooper <brian@brian-cooper.com>
2025-12-03 21:26:57 -0800

    chore: format

--- src/server/functions/auth.ts
+++ src/server/functions/auth.ts
@@ -5,9 +5,8 @@ import { getAuth } from "@/lib/auth/getAuth";
 
 export const fetchSession = createServerFn().handler(async () => {
   const request = getRequest();
+
   const session = await getAuth(request);
 
-  return {
-    session,
-  };
+  return { session };
 });
