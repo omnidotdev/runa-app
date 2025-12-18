ca6a0a2f2b5bf3471c6356fc54a2280e337adf21
Brian Cooper <brian@brian-cooper.com>
2025-12-17 20:12:48 -0800

    feature(auth): redirect home on sign out

--- src/lib/auth/signOut.ts
+++ src/lib/auth/signOut.ts
@@ -1,5 +1,11 @@
 import authClient from "@/lib/auth/authClient";
 
 export const signOut = async () => {
-  await authClient.signOut();
+  await authClient.signOut({
+    fetchOptions: {
+      onSuccess: () => {
+        window.location.href = "/";
+      },
+    },
+  });
 };
