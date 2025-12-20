02455f8ed4d21d02d35078a99b68e0f927cb1cba
Brian Cooper <brian@brian-cooper.com>
2025-12-20 02:01:05 -0800

    fix(email): use support domain

--- src/server/functions/emails.ts
+++ src/server/functions/emails.ts
@@ -4,7 +4,11 @@ import { z } from "zod";
 
 import { WorkspaceInvitation } from "@/components/emails";
 import app from "@/lib/config/app.config";
-import { BASE_URL, isDevEnv } from "@/lib/config/env.config";
+import {
+  BASE_URL,
+  FROM_EMAIL_ADDRESS,
+  TO_EMAIL_ADDRESS,
+} from "@/lib/config/env.config";
 
 export const inviteSchema = z.object({
   inviterEmail: z.email(),
@@ -22,8 +26,8 @@ export const sendInviteEmail = createServerFn({ method: "POST" })
       data;
 
     const { data: email, error } = await resend.emails.send({
-      from: `${app.name} Support <${isDevEnv ? "onboarding@resend.dev" : inviterEmail}>`,
-      to: isDevEnv ? "delivered@resend.dev" : recipientEmail,
+      from: `${app.name} Support <${FROM_EMAIL_ADDRESS}>`,
+      to: TO_EMAIL_ADDRESS || recipientEmail,
       subject: `You have been invited to join the ${workspaceName} workspace on ${app.name}`,
       react: WorkspaceInvitation({
         inviterUsername,
