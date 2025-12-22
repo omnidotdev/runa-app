42f396e78c93b6ecdb3722d110d19c98d470cbc5
Brian Cooper <brian@brian-cooper.com>
2025-12-21 18:36:01 -0800

    chore: update comments, move deps

--- src/lib/auth/signIn.ts
+++ src/lib/auth/signIn.ts
@@ -1,10 +1,18 @@
 import authClient from "@/lib/auth/authClient";
 
+interface SignInParams {
+  /** Redirect URL. */
+  redirectUrl: string;
+  // TODO: handle sign-up action if Better Auth supports it
+  // action?: "sign-up";
+}
+
 /**
  * Sign in with OAuth2 provider.
  */
-const signIn = async ({ redirectUrl }: { redirectUrl: string }) => {
+const signIn = async ({ redirectUrl }: SignInParams) => {
   await authClient.signIn.oauth2({
+    // TODO env var/derive for the self-hosting fam
     providerId: "omni",
     callbackURL: redirectUrl,
   });
