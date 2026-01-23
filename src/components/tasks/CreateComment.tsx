54396941df7cddc7a91290dcf39d220be5923932
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2026-01-22 19:07:22 -0600

    fix(task): edit styles to unblock commenting

--- src/components/tasks/CreateComment.tsx
+++ src/components/tasks/CreateComment.tsx
@@ -63,7 +63,7 @@ const CreateComment = () => {
             editorApi={editorApi}
             onUpdate={({ getHTML }) => field.handleChange(getHTML())}
             placeholder="Add a comment..."
-            className="field-sizing-content flex min-h-32 w-full rounded-xl border px-3 py-2 text-sm shadow-xs outline-none transition-[color,box-shadow] placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40"
+            className="flex min-h-32 w-full rounded-xl border text-sm shadow-xs outline-none transition-[color,box-shadow] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40"
             skeletonClassName="h-[128px] w-full"
           />
         )}
