a6a2bed6ddb5ce9165481ac763d66df302e90ae9
Brian Cooper <brian@brian-cooper.com>
2026-03-05 13:38:43 -0500

    fix(projects): add bg-background to public board main container

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -205,7 +205,7 @@ function AppLayout() {
           </div>
         </header>
 
-        <main className="flex min-h-0 flex-1 flex-col overflow-hidden">
+        <main className="flex min-h-0 flex-1 flex-col overflow-hidden bg-background">
           <Outlet />
         </main>
       </div>
